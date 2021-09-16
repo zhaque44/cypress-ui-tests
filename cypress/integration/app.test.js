12caeea37b063dbc4cc62afa8bc7e8e4274f2498
Zubair Haque <haque.zubair@gmail.com>
2021-09-16 12:49:57 -0500

    update

--- cypress/integration/app.test.js
+++ cypress/integration/app.test.js
@@ -1,15 +1,15 @@
 describe('Imagine Homepage ', () => {
     beforeEach(() => {
 
-      cy.setCookie('imagine-np', Cypress.env('IMAGINE_TOKEN'), {
+      cy.setCookie('production', Cypress.env('TOKEN'), {
         domain: 'velocity-np.ag',
       })
       
-      cy.visit('https://velocity-np.ag/imagine#')
+      cy.visit('https://base-url/page#')
       cy.wait(18000)
     })
   
     it('loads ', () => {
-      cy.get('a[href*="/imagine/directory/albums"]').should('be.visible')
+      cy.get('a[href*="/path/to/link"]').should('be.visible')
     })
   })
