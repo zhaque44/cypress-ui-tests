96f5d08e702718ea33d70cc54c42a00c8c9735d8
Zubair Haque <haque.zubair@gmail.com>
2021-09-16 12:54:50 -0500

    generate token

--- cypress/generateToken.js
+++ cypress/generateToken.js
@@ -2,7 +2,7 @@ const {ConfidentialClientApplication} = require('@azure/msal-node')
 
 new ConfidentialClientApplication({
   auth: {
-    authority: 'https://login.microsoftonline.com/fcb2b37b-5da0-466b-9b83-0014b67a7c78',
+    authority: 'https://login.microsoftonline.com/your-id',
     clientId: '',
     clientSecret: '',
   },
